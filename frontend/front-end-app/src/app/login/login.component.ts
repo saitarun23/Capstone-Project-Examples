61ba45a04859fb7470ff26e76466007e958e4286
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-10-20 23:29:23 +0530

    done

--- frontend/front-end-app/src/app/login/login.component.ts
+++ frontend/front-end-app/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { LoginService } from '../login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+msg:string="";
+  loginRef=new FormGroup({
+    emaild:new FormControl(),
+    password:new FormControl(),
+    typeofuser:new FormControl()
+  })
+
+  constructor(public ls:LoginService,public router:Router){}  // DI for LoginService 
+
+  signIn():void{
+
+    let login=this.loginRef.value;
+
+    this.ls.signIn(login).subscribe({
+      next:(result:any)=>{
+
+      },
+    error:(error:any)=> {
+      console.log(error)
+    },
+    complete:()=>{
+      console.log("SignIn done!")
+    } 
+    })
+    this.loginRef.reset();
+  }
+
 
 }
