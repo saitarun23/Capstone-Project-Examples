4a9d79b1693c4406b04d03b5fc047fabaa793cb9
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-10-20 21:16:28 +0530

    first commit

---
+++ backend/backend-micro-service-app/src/main/java/backend/com/service/LoginService.java
@@ -0,0 +1,51 @@
+package backend.com.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import backend.com.entity.Login;
+import backend.com.repository.LoginRepository;
+
+@Service
+public class LoginService {
+
+	@Autowired
+	LoginRepository loginRepository;
+	
+	public String SignIn(Login login) {
+		Optional<Login> result = loginRepository.findById(login.getEmailid());
+		if(result.isPresent()) {
+			Login ll = result.get();
+			if(ll.getPassword().equals(login.getPassword())) {
+					if(ll.getTypeofuser().equals("admin") && login.getTypeofuser().equals("admin")) {
+						return "Admin login successfully";
+					}else if(ll.getTypeofuser().equals("customer") && login.getTypeofuser().equals("customer")){
+						return "Customer login successfully";
+					}else {
+						return "type of user wrong";
+					}
+			}else {
+				return "Password is wrong";
+			}
+		}else {
+			
+			return "EmailId is wrong";
+		}
+	}
+	public String SignUp(Login login) {
+		if(login.getTypeofuser().equals("admin")) {
+			return "You can't create admin login";
+		}else {
+			Optional<Login> result = loginRepository.findById(login.getEmailid());
+			if(result.isPresent()) {
+				return "Account already exists";
+			}else {
+				loginRepository.save(login);
+				return "Account created successfully";
+			}
+		}
+	}
+	
+}
