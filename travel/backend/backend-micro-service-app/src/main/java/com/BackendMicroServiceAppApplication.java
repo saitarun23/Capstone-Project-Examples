d88d5efc26fab0d8521351bb80b860fc2ce1a8e2
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-10-21 14:34:31 +0530

    done

--- travel/backend/backend-micro-service-app/src/main/java/com/BackendMicroServiceAppApplication.java
+++ travel/backend/backend-micro-service-app/src/main/java/com/BackendMicroServiceAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackEndMicroServiceAppApplication {
+public class BackendMicroServiceAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackEndMicroServiceAppApplication.class, args);
+		SpringApplication.run(BackendMicroServiceAppApplication.class, args);
 	}
 
 }
