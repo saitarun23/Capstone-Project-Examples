859a737bae2a8d84ee8eec096f1c2386c30c48fd
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-10-21 18:07:32 +0530

    done

--- travel/frontend/front-end-app/src/app/app-routing.module.ts
+++ travel/frontend/front-end-app/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { AdmindashboardComponent } from './admindashboard/admindashboard.component';
+import { CustomerdashboardComponent } from './customerdashboard/customerdashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:LoginComponent},
+  {path:"signup",component:SignupComponent},
+  {path:"login",redirectTo:"",pathMatch:"full"},
+  {path:"admin",component:AdmindashboardComponent},
+  {path:"customer",component:CustomerdashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
