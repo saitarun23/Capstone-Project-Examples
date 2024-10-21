5e2d678efe88bfa287f9428fefac3fd888b22f3f
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-10-21 18:02:02 +0530

    done

--- travel/frontend/front-end-app/src/app/login/login.component.ts
+++ travel/frontend/front-end-app/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { LoginService } from '../login.service';
+import { Login } from '../login';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +11,40 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  loginRef=new FormGroup({
+    emailid:new FormControl(),
+    password:new FormControl(),
+    typeofuser:new FormControl()
+  });
+
+  msg:string="";
+
+  constructor(public ls:LoginService,public router:Router){}    // DI for LoginService
+
+  signIn():void{
+    let login=this.loginRef.value;
+      //console.log(login);
+    this.ls.signIn(login).subscribe({
+
+      next:(result:any)=>{
+
+        if(result=="Admin login successfully"){
+          this.router.navigate(["admin"],{skipLocationChange:true})
+        }else if(result=="Customer login successfully"){
+          this.router.navigate(["customer"],{skipLocationChange:true})
+        }else{
+          this.msg=result;
+        }
+      },
+
+      error:(error:any)=>{
+        console.log(error)
+      },
+
+      complete:()=>{
+        console.log("SignIn done!")
+      }
+    })
+    this.loginRef.reset();
+  }
 }
--- travel/frontend/front-end-app/src/app/signup/signup.component.ts
+++ travel/frontend/front-end-app/src/app/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,4 +9,32 @@ import { Component } from '@angular/core';
 })
 export class SignupComponent {
 
+  loginRef=new FormGroup({
+    emailid:new FormControl(),
+    password:new FormControl(),
+    typeofuser:new FormControl()
+  });
+
+  msg:string="";
+
+  constructor(public ls:LoginService){}       // DI for LoginService
+
+  signUp():void{
+    let login=this.loginRef.value;
+
+    this.ls.signUp(login).subscribe({
+      next:(result:any)=>{
+        this.msg=result;
+      },
+      error:(error:any)=>{
+        console.log(error)
+      },
+      complete:()=>{
+        console.log("SignUp done!")
+      }
+    })
+    console.log(login);
+
+    this.loginRef.reset();
+  }
 }
