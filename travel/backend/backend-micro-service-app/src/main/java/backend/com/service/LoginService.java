e905508fee98933053c9b8722461eeb0f99dc84d
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-10-21 14:59:21 +0530

    done

--- travel/backend/backend-micro-service-app/src/main/java/backend/com/service/LoginService.java
+++ travel/backend/backend-micro-service-app/src/main/java/backend/com/service/LoginService.java
@@ -14,6 +14,27 @@ public class LoginService {
 	@Autowired
 	LoginRepository loginRepository;
 	
+	public String SignIn(Login login) {
+		Optional<Login> result=loginRepository.findById(login.getEmailid());
+		if(result.isPresent()) {
+			Login ll=result.get();
+			if(ll.getPassword().equals(login.getPassword())) {
+				if(ll.getTypeofuser().equals("admin") && login.getTypeofuser().equals("admin")) {
+					return "Admin Login Successfully";
+				}else if(ll.getTypeofuser().equals("customer") && login.getTypeofuser().equals("customer")) {
+					return "Customer login successfully";
+				}else {
+					return "type of user wrong";
+				}
+			}else {
+				return "Password is wrong";
+			}
+		}else {
+			return "EmailId is wrong";
+		}
+	}
+	
+	
 	public String SignUp(Login login) {
 		if(login.getTypeofuser().equals("admin")) {
 			return "You can't create admin login";
@@ -22,8 +43,8 @@ public class LoginService {
 			if(result.isPresent()) {
 				return "Account already exists";
 			}else {
-			loginRepository.save(login);
-			return "Account Create Successfully";
+					loginRepository.save(login);
+					return "Account Create Successfully";
 			}
 		}
 	}
